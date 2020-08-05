e349009fbe457cdc1be51565987c3ae839a21657
Liazzz <godblessed9926@gmail.com>
2020-08-05 11:59:23 +0600

    add project

--- makers_test/account/views.py
+++ makers_test/account/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
-# Create your views here.
+from makers_test.account.forms import RegistrationForm
+
+
+class Regiterview(CreateView):
+    model = User
+    form_class = RegistrationForm
+    template_name = 'account/registration.html'
