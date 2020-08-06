8b1a02a3d6120cca479336041a944219daadde36
Liazzz <godblessed9926@gmail.com>
2020-08-06 13:46:14 +0600

    Fix gunicorn

--- makers_test/settings.py
+++ makers_test/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'demo-djangotest.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'demo-ilyadjangotest.herokuapp.com']
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
